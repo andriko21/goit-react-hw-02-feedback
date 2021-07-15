492ec057e8ff8e9150a3b5dfda96aa68f3ed42e2
andriko21 <75207634+andriko21@users.noreply.github.com>
2021-07-15 18:46:43 +0300

    Зробив все крім відображення повідомлення коли статистика = 0

---
+++ src/components/Section/Section.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from 'prop-types'
+import styles from '../Section/Section.module.css'
+const Section = ({ title, children }) => (
+  <section className={styles.Section}>
+    <h1 className={styles.title}>Please leave feedback</h1>
+    {children}
+  </section>
+);
+
+Section.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+Section.defaultProps = {
+    children: ' ',
+}
+
+export default Section;
